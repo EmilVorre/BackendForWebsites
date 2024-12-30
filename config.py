41c6a90a5b2f4dfb1482751b23c9a1eae6438804
VorHerre <evorre23@student.aau.dk>
2024-12-30 22:36:22 +0100

    Update configuration settings for backend, including allowed origins and host address

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Settings:
     PROJECT_NAME: str = "Backend for EmilVorre.dk"
     PROJECT_VERSION: str = "1.0.0"
-    ALLOWED_ORIGINS: list = ["emilvorre.dk", "localhost:5173", "94.145.239.14"]
+    ALLOWED_ORIGINS: list = ["emilvorre.dk", "localhost",]
     HOST: str = "185.172.173.25"
     PORT: int = 8000
 
